0ec3cb9adec9e25c45e2bc3426f78d15de5ae52b
1102137105 <1102137105@gm.kuas.edu.tw>
2016-03-22 21:05:36 +0800

    111

---
+++ HW1/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0317.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrder("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            Console.WriteLine(ViewBag.CustName);
+            return View();
+        }
+    }
+}
---
+++ HW1/Models/OrderService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class OrderService
+    {   
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+        /// <summary>
+        /// 刪除訂單
+        /// </summary>
+        public void DeleteOrder()
+        {
+
+        }
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Models.Order GetOrder(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "gss";
+            result.CustName = "加一";
+            return result;
+        }
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+
+    }
+}
